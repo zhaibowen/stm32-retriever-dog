fcff1aeaf24c179e987efd9dd833249470f14ccc
leeyun <leeyun.bw@gmail.com>
2025-08-22 11:46:24 +0800

    m

--- 机器狗/stm32f103c6t6/Objects/dog_utils.d
+++
@@ -1,3 +0,0 @@
-.\objects\dog_utils.o: Dog\dog_utils.c
-.\objects\dog_utils.o: Dog\dog_utils.h
-.\objects\dog_utils.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
--- 机器狗/stm32f103c6t6/user/main.c
+++ 机器狗/stm32f103c6t6/user/main.c
@@ -11,10 +11,11 @@
 #include "Servo.h"
 
 uint8_t Buf[32] = {0};
+uint16_t Lx, Ly, Rx, Ry;
+uint8_t Key0, Key1, Key2, Key3, Key4, Key5;
 float i2c_freq = 400000; // Hz
 
-void remote_control_run(float* target_ang);
-void commander_run(uint8_t* debug_flag);
+void remote_control_run(void);
 
 int main(void)
 {
@@ -27,17 +28,8 @@ int main(void)
 	I2C_Init_(I2C1, i2c_freq);
 	MPU6050_Init(&MPU, I2C1);
 	printf("MPU ID: %x\r\n", MPU6050_GetID(&MPU));
-	Servo_SetAngle(0, 90);
-	Servo_SetAngle(1, 90);
-	Servo_SetAngle(2, 90);
-	Servo_SetAngle(3, 90);
-	Servo_SetAngle(4, 90);
-	Servo_SetAngle(5, 90);
-	Servo_SetAngle(6, 90);
-	Servo_SetAngle(7, 90);
-	delay_ms(1000);            //Wait for the system to stabilize
 
-	if (0) {
+	if (0) { // 陀螺仪校准
 		MPU6050_CalcGyroOffsets(&MPU);
 		return 0;
 	}
@@ -45,59 +37,90 @@ int main(void)
 	int pknt = 0;
 	unsigned long timestamp_prev = 0;
 	uint8_t debug_flag = 1;
-	float target_ang = 90;
+	uint8_t init_ang = 90;
+	uint8_t init_ang_arr[4][2] = {{94, 91}, {87, 89}, {88, 91}, {94, 93}};
+	uint8_t leg_number = 0, leg_part = 0;
+	uint8_t calibration = 0;
 	while(1) {
-		MPU6050_Update(&MPU);		//获取MPU6050的数据
-		float mpu_pitch = -MPU.angleY;
-		
-		Servo_SetAngle(0, target_ang);
-
-		if (debug_flag) {
-			unsigned long now_us;
-			float Ts;
-			now_us = time_cntr;
-			if (now_us < timestamp_prev) {
-				Ts = (ulong_max_value - timestamp_prev + now_us) * 1e-5;
-			} else {
-				Ts = (now_us - timestamp_prev) * 1e-5;
+		remote_control_run();
+		if (calibration == 0) {
+			if (Key0) { // 舵机标定
+				leg_number = 0;
+				leg_part = 0;
+				for (int i = 0; i < 4; ++i) {
+					for (int j = 0; j < 2; ++j) {
+						init_ang_arr[i][j] = init_ang;
+						Servo_SetAngle(i*2+j, init_ang_arr[i][j]);
+					}
+				}
+				calibration = 1;
+				printf("servo calibration, init angle: 90\r\n");
 			}
-			timestamp_prev = now_us;
-
-			if (pknt == 0) {
-				printf("%.5f, %.4f, %.4f, %.4f\r\n", Ts, mpu_pitch, MPU.angleAccY, MPU.gyroY);
-				pknt = 100;
+		} else if (calibration) { // 进入舵机标定
+			uint8_t press_flag = 0;
+			if (Key1) { // 选择哪条腿
+				press_flag = 1;
+				leg_number = (leg_number + 1) % 4;
+			} else if (Key2) { // 选择大腿或小腿
+				press_flag = 1;
+				leg_part = (leg_part + 1) % 2;
+			} else if (Key3) { // 舵机角度 + 1
+				press_flag = 1;
+				init_ang_arr[leg_number][leg_part] += 1;
+			} else if (Key4) { // 舵机角度 - 1
+				press_flag = 1;
+				init_ang_arr[leg_number][leg_part] -= 1;
+			} else if (Key5) { // 退出舵机标定
+				calibration = 0;
+				printf("exit servo calibration\r\n");
+				for (int i = 0; i < 4; ++i) {
+					for (int j = 0; j < 2; ++j) {
+						printf("    calibration leg %d, part %d, angle %d\r\n", i, j, init_ang_arr[i][j]);
+					}
+				}
 			}
-			delay_ms(3);
-			pknt -= 1;
+			if (press_flag) {
+				Servo_SetAngle(leg_number*2+leg_part, init_ang_arr[leg_number][leg_part]);
+				printf("    choose leg %d, part %d, current angle %d\r\n", leg_number, leg_part, init_ang_arr[leg_number][leg_part]);
+			}
 		}
-		remote_control_run(&target_ang);
-		commander_run(&debug_flag);
+		delay_ms(200);
+
+		// MPU6050_Update(&MPU);		//获取MPU6050的数据
+		// float mpu_pitch = -MPU.angleY;
+		// if (debug_flag) {
+		// 	unsigned long now_us;
+		// 	float Ts;
+		// 	now_us = time_cntr;
+		// 	if (now_us < timestamp_prev) {
+		// 		Ts = (ulong_max_value - timestamp_prev + now_us) * 1e-5;
+		// 	} else {
+		// 		Ts = (now_us - timestamp_prev) * 1e-5;
+		// 	}
+		// 	timestamp_prev = now_us;
+
+		// 	if (pknt == 0) {
+		// 		printf("%.5f, %.4f, %.4f, %.4f\r\n", Ts, mpu_pitch, MPU.angleAccY, MPU.gyroY);
+		// 		pknt = 100;
+		// 	}
+		// 	delay_ms(3);
+		// 	pknt -= 1;
+		// }
 	}
 }
 
-void remote_control_run(float* target_ang) {
-	uint16_t X;
+void remote_control_run(void) {
 	if (NRF24L01_Get_Value_Flag() == 0) {
 		NRF24L01_GetRxBuf(Buf);
-		X = Buf[1] * 256 + Buf[0];
-		if (X >= 3000) {
-			*target_ang = 50;
-		} else if (X <= 1000) {
-			*target_ang = 130;
-		}
-	}
-}
-
-void commander_run(uint8_t* debug_flag) {
-	if((USART_RX_STA&0x8000) != 0)
-	{
-		switch(USART_RX_BUF[0])
-		{
-			case 'D': // debug
-				*debug_flag = atoi((const char *)(USART_RX_BUF+1));
-				printf("debug_flag=%d\r\n", *debug_flag);
-				break;
-		}
-		USART_RX_STA=0;
+		Lx = Buf[1] * 256 + Buf[0];
+		Ly = Buf[3] * 256 + Buf[2];
+		Rx = Buf[5] * 256 + Buf[4];
+		Ry = Buf[7] * 256 + Buf[6];
+		Key0 = Buf[8] ^ 1;
+		Key1 = Buf[9] ^ 1;
+		Key2 = Buf[10] ^ 1;
+		Key3 = Buf[11] ^ 1;
+		Key4 = Buf[12] ^ 1;
+		Key5 = Buf[13] ^ 1;
 	}
 }
